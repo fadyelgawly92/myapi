b245081702793e365f34dc4c39aaa9c3655192c3
fadyelgawly92 <fadyraafatelgawly@gmail.com>
2018-09-04 23:52:21 +0200

    middleware route grp done for routes,sqlite testing connection added in the config database file and the environmental variable in the phpunit.xml , setup method written in test case class and test is added to the composer.json, and the factories are done to create objects using faker for the user and the article

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -3,8 +3,15 @@
 namespace Tests;
 
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
 
 abstract class TestCase extends BaseTestCase
 {
-    use CreatesApplication;
+    use CreatesApplication,DatabaseMigrations;
+
+    public function setup()
+    {
+        parent::setUp();
+        Artisan::call('db:seed');
+    }
 }
